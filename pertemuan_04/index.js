8aa0212fee701cbec9b4400ca5f1837776b61c68
GlafertPrigerio <s21810047@student.unklab.ac.id>
2020-08-27 14:21:29 +0800

    1st Commit

---
+++ pertemuan_04/index.js
@@ -0,0 +1,53 @@
+/* 
+1. function Declaration
+*/
+function penjumlahanDuaBujurSangkar(a, b) {
+    let luasA = a * a;
+    let luasB = b * b;
+
+    let total = luasA + luasB;
+
+    return total;
+}
+const hasil = penjumlahanDuaBujurSangkar (1 , 3)
+console.log(hasil);
+
+/* 
+Refactoring
+
+menghilangkan bagian yang tidak perlu
+*/
+
+function penjumlahanDuaBujurSangkar(a, b) {
+
+    let total = a * a + b * b;
+
+    return total;
+}
+const hasil2 = penjumlahanDuaBujurSangkar (1 , 3)
+console.log(hasil);
+
+function tambah (a, b){
+    return a + b;
+}
+function kali(a , b){
+let hasilKali = a * b;
+
+return hasilKali;
+}
+console.log(tambah(kali(5,2),kali (10,2)));
+
+/* hoisting = bisa di di atas deklarasi
+---------------------------------------
+ */
+
+
+/* function Expresion 
+-----------------------------------------*/
+
+const luasSegitiga = function Aritmetik (a, b , c) {
+const stengah = 0,5;
+    const total = stengah * b * c;
+    return total;
+}
+console.log(luasSegitiga);
