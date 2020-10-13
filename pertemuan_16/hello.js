9b36dac33f55e952354453d6994caa0b6dbafbac
GlafertPrigerio <69837693+GlafertPrigerio@users.noreply.github.com>
2020-10-13 13:53:01 +0800

    1st commit

---
+++ pertemuan_16/hello.js
@@ -0,0 +1,9 @@
+//Export Module
+//  const hello = (user) => {
+//     console.log (`Hello, Nama saya ${user}`);
+// }
+// export default hello;
+
+export const user = (name) =>{
+    console.log(`Nama saya adalah ${name}`);
+}
---
+++ pertemuan_16/index16.js
@@ -0,0 +1,11 @@
+//Import Module
+// import {hello} from './hello.js';
+//import * as helper from './hello.js'
+
+//import menggunakan default
+// import func1 from './hello.js'
+// hello("Glafert");
+
+import {user} from './hello.js'
+user("Glafert");
+
