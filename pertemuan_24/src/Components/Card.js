88ab077386756e2d6261d184d79aaeb7b852be24
GlafertPrigerio <69837693+GlafertPrigerio@users.noreply.github.com>
2020-11-19 14:21:31 +0800

    1st Commit

---
+++ pertemuan_24/src/Components/Card.js
@@ -0,0 +1,14 @@
+  
+import React from 'react'
+
+
+export const Card = ({name, username, email, phone}) =>{
+    return (
+        <div className="card-container">
+            <p>{name}</p>
+            <p>{username}</p>
+            <p>{email}</p>
+            <p>{phone}</p>
+        </div>
+    )
+}
---
+++ pertemuan_24/src/Components/Header.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Header = () =>{
+    return (
+      <>  
+        <h1>Basic React</h1>
+        <h2>Component React</h2>
+      </>
+    );
+  };
+
+export default Header;
