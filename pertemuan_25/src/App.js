0a132579545be2596c4b2f1c7091573a47b6bc91
GlafertPrigerio <69837693+GlafertPrigerio@users.noreply.github.com>
2020-11-30 16:49:46 +0800

    pertemuan_25-26

---
+++ pertemuan_25/src/App.js
@@ -0,0 +1,62 @@
+import React, {Component, useEffect, useState} from 'react'
+import Card from "./components/card"
+import "./App.css"
+import axios from 'axios';
+
+
+
+// class App extends Component (
+//     state = {
+//         users: [],
+//     };
+
+//     componentDidMount(){
+//         axios
+//             .get((response) => this.setState({users: response.data}));
+//             .
+
+//     }
+//     render(){
+//         return(
+//             {
+//                 this.state.users.map((item)=>{
+//                     return(
+//                         <Card
+//                             name={item.name}
+//                             username={item.username}
+//                             email={item.email}
+//                             phone={item.phone} 
+//                         />
+//                     )
+//                 })
+//             }
+//         )
+//     }
+// )
+
+
+//function
+const App = () =>{
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        axios
+            .get("https://jsonplaceholder.typicode.com/users")
+            .then((response) => setUsers(response.data))
+    }, [])
+    console.log(users)
+    return(
+        <>
+            {
+                users.map((item) =>{
+                    return(
+                        <Card
+                            name={item.name}
+                            username={item.username}
+                            email={item.email}
+                            phone={item.phone} 
+                        />
+                    )
+                })
+            }
+        </>
